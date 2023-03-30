c5d94fbc11331150b25714a79d76dbda697e3712
flareflo <github@flareflo.dev>
2023-03-31 01:59:10 +0200

    Remove another redundant fn

--- src/subcommands/unpack_raw_blk.rs
+++ src/subcommands/unpack_raw_blk.rs
@@ -15,7 +15,6 @@ use crate::task_queue::FileTask;
 
 // This is the entry-point
 pub fn unpack_raw_blk(args: &ArgMatches) -> Result<(), CliError> {
-
 	// This should be infallible
 	let input_dir = args.get_one::<String>("Input directory").ok_or(CliError::RequiredFlagMissing)?;
 	let input_read_dir = fs::read_dir(input_dir)?;
@@ -24,20 +23,13 @@ pub fn unpack_raw_blk(args: &ArgMatches) -> Result<(), CliError> {
 	let mut prepared_files = vec![];
 	read_recurse_folder(&mut prepared_files, input_read_dir).unwrap();
 
-	translate_files(input_dir, prepared_files);
-	Ok(())
-}
-
-
-fn translate_files(base_path: &str, pile: Vec<(PathBuf, Vec<u8>)>) -> Vec<String> {
-
 	// The shared name map must always reside at the top level
 	info!("Reading NM file");
-	let nm = fs::read(format!("{}/nm", base_path)).unwrap();
+	let nm = fs::read(format!("{}/nm", input_dir)).unwrap();
 
 	// Dict files have hashed prefixes, so we find them via their file-name suffix and or magic bytes
 	info!("Autodetecting dict file");
-	let (name, dict) = find_dict(base_path).unwrap();
+	let (name, dict) = find_dict(input_dir).unwrap();
 	info!("Found dict at {}", name);
 
 	info!("Preparing shared indexes");
@@ -52,9 +44,9 @@ fn translate_files(base_path: &str, pile: Vec<(PathBuf, Vec<u8>)>) -> Vec<String
 	bar.set_style(
 		ProgressStyle::with_template(" [{elapsed}/{eta}] [{wide_bar:.cyan/blue}] {percent}% {pos}/{len}").unwrap().progress_chars("#>-")
 	);
-	bar.set_length(pile.len() as u64);
+	bar.set_length(prepared_files.len() as u64);
 
-	let out = pile.into_iter().map(|file| {
+	let out = prepared_files.into_iter().map(|file| {
 		let out = parse_file(file.1, arced_fd.clone(), rc_nm.clone());
 		bar.inc(1);
 		out
@@ -62,5 +54,5 @@ fn translate_files(base_path: &str, pile: Vec<(PathBuf, Vec<u8>)>) -> Vec<String
 				  .collect::<Vec<_>>();
 	bar.finish();
 
-	out
+	Ok(())
 }
