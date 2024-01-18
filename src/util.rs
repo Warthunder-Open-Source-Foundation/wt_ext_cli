e2369c94a7bd1c19db56fa550ea7eb98acffd15b
FlareFlo <mail@flareflo.dev>
2024-01-18 13:51:33 +0100

    Fix bug where Crlf writer incorrectly returns count

--- src/util.rs
+++ src/util.rs
@@ -7,29 +7,39 @@ macro_rules! context {
 }
 
 
-pub struct CrlfWriter<W: io::Write> {
-	inner: W,
+pub enum CrlfWriter<W: io::Write> {
+	Enabled(W),
+	Disabled(W),
 }
 
 impl<W: io::Write> io::Write for CrlfWriter<W> {
 	// Replace any \n to \r\n
 	fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-		let mut count = 0;
+		match self {
+			CrlfWriter::Enabled(inner) => {
+				let mut count = 0;
 
-		for &byte in buf.iter() {
-			if byte == b'\n' {
-				// Write \r before \n
-				self.inner.write_all(&[b'\r'])?;
-				count += 1;
+				for &byte in buf.iter() {
+					if byte == b'\n' {
+						// Write \r before \n
+						count += 1;
+						inner.write_all(b"\r\n")?;
+					} else {
+						count += inner.write(&[byte])?;
+					}
+				}
+
+				Ok(count)
 			}
-			self.inner.write_all(&[byte])?;
-			count += 1;
+			CrlfWriter::Disabled(inner) => {
+				inner.write(buf)
+			}
 		}
-
-		Ok(count)
 	}
 
 	fn flush(&mut self) -> io::Result<()> {
-		self.inner.flush()
+		match self {
+			CrlfWriter::Enabled(i) | CrlfWriter::Disabled(i) => i.flush()
+		}
 	}
 }
