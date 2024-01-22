58242c7c4f28d456a05a17edddc061eafe64a1a4
FlareFlo <mail@flareflo.dev>
2024-01-22 16:25:07 +0100

    Add OsStr import for AVIF to DDS feature in unpack_vromf

--- src/subcommands/unpack_vromf.rs
+++ src/subcommands/unpack_vromf.rs
@@ -143,6 +143,7 @@ fn parse_and_write_one_vromf(
 			}
 			#[cfg(feature = "avif2dds")]
 			if avif2dds {
+				use std::ffi::OsStr;
 				if file.0.extension() == Some(&OsStr::new("avif")) {
 					let image = image::load_from_memory_with_format(&file.1, ImageFormat::Avif);
 					match image {
