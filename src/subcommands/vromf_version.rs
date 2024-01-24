e900fb7f0357262a37b543abab5af036745a191c
FlareFlo <mail@flareflo.dev>
2024-01-24 14:02:15 +0100

    Ensure vromf_version errors surface, update wt_blk dependency.

--- src/subcommands/vromf_version.rs
+++ src/subcommands/vromf_version.rs
@@ -1,9 +1,10 @@
 use std::fs;
+use std::iter::{once};
 use std::path::PathBuf;
 use std::str::FromStr;
 use clap::ArgMatches;
+use serde_json::{json, Map, Value};
 use wt_blk::vromf::VromfUnpacker;
-use wt_version::Version;
 use crate::error::CliError;
 
 pub fn vromf_version(args: &ArgMatches) -> color_eyre::Result<()> {
@@ -14,22 +15,24 @@ pub fn vromf_version(args: &ArgMatches) -> color_eyre::Result<()> {
 
 	let versions: Vec<_> = if parsed_input_dir.is_file() {
 		let unpacker = VromfUnpacker::from_file((parsed_input_dir.clone(), fs::read(&parsed_input_dir)?))?;
-		vec![(parsed_input_dir.file_name().unwrap().to_string_lossy().to_string(), unpacker.latest_version().ok())]
+		vec![(parsed_input_dir.file_name().unwrap().to_string_lossy().to_string(), unpacker.latest_version()?)]
 	} else {
 		let dir = parsed_input_dir.read_dir()?;
 		let mut versions = vec![];
 		for file in dir {
 			let p = file?.path();
 			let unpacker = VromfUnpacker::from_file((p.clone(), fs::read(&p)?))?;
-			versions.push((p.file_name().unwrap().to_string_lossy().to_string(), unpacker.latest_version().ok()));
+			versions.push((p.file_name().unwrap().to_string_lossy().to_string(), unpacker.latest_version()?));
 		}
 		versions
-	}.into_iter().map(|(mut i,e)|{
-		i.push(' ');
-		i.push_str(&e.map(|e|e.to_string()).unwrap_or("0.0.0.0".to_owned()));
-		i
-	}).collect();
-	println!("{}", serde_json::to_string_pretty(&versions)?);
+	};
+	let json = Value::Array(
+		versions.into_iter()
+			.map(|e|{
+				Value::Object(Map::from_iter(once((e.0, json!(e.1.map(|e|e.to_string()))))))
+			}).collect()
+	);
+	println!("{}", serde_json::to_string_pretty(&json)?);
 
 	Ok(())
 }
